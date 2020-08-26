1b4ba571389fb3f3aa2e74a9e85992d61b98ce01
akwanmaroso <akwanmaroso@gmail.com>
2020-08-26 17:43:13 +0800

    feat: add feature search repository

--- search.go
+++ search.go
@@ -1,22 +1,64 @@
 package pork
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/akwanmaroso/devops-go/pork/nap"
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 )
+
+type SearchResponse struct {
+	Result []*SearchResult `json:"result"`
+}
+
+type SearchResult struct {
+	FullName string `json:"full_name"`
+}
 
 var SearchCmd = &cobra.Command{
 	Use: "search",
 	Short: "search for github repository by keyword",
 	Run: func(cmd *cobra.Command, args []string) {
-		repositoryList := SearchByKeyword(args)
-		for _, repository := range repositoryList {
-			fmt.Println(repository)
+		if err := SearchByKeyword; err != nil {
+			log.Fatalln("Search Failed:", err)
 		}
 	},
 }
 
-func SearchByKeyword(keyword []string) []string {
-	return []string{"myrepository"}
+func SearchByKeyword(keyword []string) error {
+	return GithubAPI().Call("search", map[string]string{
+		"query": strings.Join(keyword, "+"),
+	})
+}
+
+func SearchSuccess(resp *http.Response) error {
+	defer resp.Body.Close()
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	respContent := SearchResponse{}
+	json.Unmarshal(content, &respContent)
+	for _, item := range respContent.Result {
+		fmt.Println(item.FullName)
+	}
+	return nil
+}
+
+func SearchDefaultRouter(resp *http.Response) error {
+	return fmt.Errorf("status code %d", resp.StatusCode)
+}
+
+func GetSearchResource() *nap.RestResource {
+	searchRouter := nap.NewRouter()
+	searchRouter.DefaultRouter = SearchDefaultRouter
+	searchRouter.RegisterFunc(200, SearchSuccess)
+	search := nap.NewResource("/search/repositories?q={{.query}}", "GET", searchRouter)
+	return search
 }
 
