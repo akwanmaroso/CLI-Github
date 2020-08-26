7e93abafa0486128a72ca6dbbb4339afd8f17dc0
akwanmaroso <akwanmaroso@gmail.com>
2020-08-26 17:45:41 +0800

    perf: append search feature in api

--- api.go
+++ api.go
@@ -1,1 +1,20 @@
 package pork
+
+import (
+	"github.com/akwanmaroso/devops-go/pork/nap"
+	"github.com/spf13/viper"
+)
+
+var api *nap.API
+
+func GithubAPI() *nap.API {
+	if api == nil {
+		api = nap.NewAPI("https://api.github.com")
+		token := viper.GetString("token")
+		api.SetAuth(nap.NewAuthToken(token))
+		api.AddResource("fork", GetForkResource())
+		api.AddResource("search", GetSearchResource())
+	}
+
+	return api
+}
